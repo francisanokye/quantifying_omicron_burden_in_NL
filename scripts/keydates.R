d3d1ad8900e18f610df260649108693a6fe0ee36
Francis Anokye <fanokye@mun.ca>
2025-05-08 12:40:49 -0230

    Updates

--- scripts/keydates.R
+++ scripts/keydates.R
@@ -4,8 +4,8 @@ set.seed(2025)
 start_date <- "2021-12-15"
 last_date <-"2022-05-26"
 
-## MLi: Change date for now? ASL?
-change_dates <- as.Date(c("2021-12-15", "2021-12-25", "2022-01-09", "2022-02-08", "2022-03-15"))
+## change dates for ALS
+change_dates <- as.Date(c("2021-12-15", "2021-12-25", "2022-01-04", "2022-02-08", "2022-03-15"))
 
 saveEnvironment()
 
