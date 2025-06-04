46a6407864f7476ee593aceed1cffa0108cc4e47
francisanokye <fanokye@mun.ca>
2025-06-04 16:05:24 -0230

    model parameterization with vaccination data

--- scripts/flows.R
+++ scripts/flows.R
@@ -6,20 +6,18 @@ library(shellpipes)
 flows <- list(
   N ~ N1 + N2 + N3
   , foi ~ beta * (zeta * (A1 + A2 + A3) + (I1 + I2 + I3)) / N
-  , double_vac ~ v2 * (1 - S1 / v2_max)
-  #, double_vac ~ ((K2 - L2) * r2 * exp((mid2 - time_step(0)) * r2)) / (1 + exp((mid2 - time_step(0)) * r2))^2
-  , booster_shot ~ v3 * (1 - V2 / v3_max)
+  , double_vac ~ v2 
+  , booster_shot ~ v3 
 
-  #, mp_per_capita_flow("S1", "E1", "kappa1 * foi", "incS")
-  , mp_absolute_flow("S1", "V2", "double_vac", "s1_v2")
-  #, mp_per_capita_flow("S1", "V2", "double_vac", "s1_v2")
+  , mp_per_capita_flow("S1", "E1", "kappa1 * foi", "incS")
+  , mp_per_capita_flow("S1", "V2", "double_vac/S1", "s1_v2")
   , mp_per_capita_flow("E1", "I1", "sigma * mu", "inc_symp1")
   , mp_per_capita_flow("E1", "A1", "sigma * (1-mu)", "inc_asymp1")
   , mp_per_capita_flow("A1", "R1", "gamma_a", "asymp_recov1")
   , mp_per_capita_flow("I1", "R1", "gamma_i", "symp_recov1")
 
   , mp_per_capita_flow("V2", "E2", "kappa2 * foi","incv2")
-  , mp_per_capita_flow("V2", "V3", "booster_shot", "v2_v3")
+  , mp_per_capita_flow("V2", "V3", "booster_shot/V2", "v2_v3")
   , mp_per_capita_flow("E2", "I2", "sigma * mu", "inc_symp2")
   , mp_per_capita_flow("E2", "A2", "sigma * (1-mu)", "inc_asymp2")
   , mp_per_capita_flow("A2", "R2", "gamma_a", "asymp_recov2")
--- scripts/timevar_spec.R
+++ scripts/timevar_spec.R
@@ -4,6 +4,10 @@ library(zoo)
 library(shellpipes)
 loadEnvironments()
 
+double_daily_vac= csvRead("daily_2vac_rate")
+
+booster_daily_vac = csvRead("daily_booster_rate")
+
 reporting_delay <- TRUE
 
 spec <- mp_tmb_model_spec(
@@ -18,10 +22,28 @@ spec <- mp_tmb_model_spec(
   , during = flows
   , default = c(params)
 )
+# we create a piecewise time-varying vaccination rate based on true data
+double_vac_changepoints = double_daily_vac$days - 1 
+double_vac_values = double_daily_vac$double_daily_rate
+
+booster_vac_changepoints = booster_daily_vac$days - 1
+booster_vac_values = booster_daily_vac$booster_daily_rate
+
+expr2 = list(v2 ~ time_var(double_vac_values, double_vac_changepoints))
+expr3 = list(v3 ~ time_var(booster_vac_values, booster_vac_changepoints))
+
+# update model specification to reflect the vaccination rates from data
+timevar_spec <- spec |> mp_tmb_insert(
+      phase = "during"
+    , at = 1
+    , expressions = c(expr2, expr3)
+    , default = list(double_vac_values = double_vac_values, booster_vac_values = booster_vac_values)
+    , integers = list(double_vac_changepoints = double_vac_changepoints, booster_vac_changepoints = booster_vac_changepoints)
+)
 
 
 if(reporting_delay){
-	timevar_spec = mp_tmb_insert_reports(spec
+	timevar_spec = mp_tmb_insert_reports(timevar_spec
 					     , incidence_name = "inc"
 					     , report_prob = 1.0
 					     , mean_delay = 14
@@ -30,6 +52,9 @@ if(reporting_delay){
 					     , report_prob_name = "report_prob"
 	)
 }
+
+print(timevar_spec)
+
 
 rdsSave(timevar_spec)
 
