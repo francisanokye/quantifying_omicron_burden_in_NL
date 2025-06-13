2489297d979f40a81a5460f77ae5eb75aba68b19
wzmli <wzmichael.li@gmail.com>
2025-06-13 14:35:05 -0400

    hacky way of getting the fits now

--- scripts/calibrate.R
+++ scripts/calibrate.R
@@ -33,17 +33,23 @@ print(seroprevdata, n=Inf)
 calibrator = mp_tmb_calibrator(
     spec = timevar_spec |> mp_hazard()
   , data = seroprevdata |> select(-dates)
-  , time = mp_sim_offset(0, 30, "steps")
+#  , time = mp_sim_offset(0, 30, "steps")
+  , time = mp_sim_offset(30, 30, "steps")
   , outputs = c(outputs)
 #  , traj = list(serop = mp_normal(sd = mp_fit(0.01))) # 0.015
   , traj = list(inc = mp_neg_bin(disp = mp_fit(0.01))) # 0.015
+#  , traj = list(inc = mp_poisson()) # 0.015
+#  , tv = mp_rbf("beta", 5, fit_prior_sd = FALSE, prior_sd = 1)
   , tv = mp_rbf("beta", 5)
+
   , par = c("beta")
 )
 
+mp_optimize(calibrator)
 if (interactive()) {
 new_spec = mp_optimized_spec(calibrator, spec_structure = "modified")
 sim = mp_simulator(new_spec, 162, c("beta", "inc"))
+# sim = mp_simulator(new_spec, 162, c("beta", "inc"))
 (sim |> mp_trajectory() |> ggplot()
   + geom_line(aes(time, value))
   + geom_point(aes(time, value), data = seroprevdata, colour = "red")
@@ -51,10 +57,57 @@ sim = mp_simulator(new_spec, 162, c("beta", "inc"))
 )
 }
 
-mp_optimize(calibrator)
 
 model_estimates = mp_tmb_coef(calibrator, conf.int = TRUE)
 print(model_estimates, digits = 2)
+
+
+beta_changepoints <- c(0,c(25,50,85))
+beta_change <- c(0.1,0.1,0.1)
+
+newspecs <- (timevar_spec 
+  |> mp_tmb_insert(phase = "during"
+  , at = 1
+#  , expressions = list(beta ~ time_var(c(beta0,beta_change,beta_last),beta_changepoints))
+  , expressions = list(beta ~ time_var(c(beta0,beta_change),beta_changepoints))
+#, expressions = list(beta ~ time_var(c(beta_change),beta_changepoints))
+
+#  , default = list(beta_change = beta_change, beta0=0.22,beta_last=0.3)
+, default = list(beta_change = beta_change, beta0=0.22)
+#, default = list(beta_change = beta_change)
+, integers = list(beta_changepoints = beta_changepoints) 
+))
+
+calibrator2 = mp_tmb_calibrator(
+  spec = newspecs |> mp_hazard()
+  , data = seroprevdata |> select(-dates)
+  #  , time = mp_sim_offset(0, 30, "steps")
+  , time = mp_sim_offset(0, 30, "steps")
+  , outputs = c(outputs)
+  #  , traj = list(serop = mp_normal(sd = mp_fit(0.01))) # 0.015
+  , traj = list(inc = mp_neg_bin(disp = mp_fit(0.01))) # 0.015
+  #  , traj = list(inc = mp_poisson()) # 0.015
+  #  , tv = mp_rbf("beta", 5, fit_prior_sd = FALSE, prior_sd = 1)
+  , par = c("beta_change")
+)
+
+
+mp_optimize(calibrator2)
+if (interactive()) {
+  new_spec = mp_optimized_spec(calibrator2, spec_structure = "modified")
+  sim = mp_simulator(new_spec, 162, c("beta", "inc"))
+  # sim = mp_simulator(new_spec, 162, c("beta", "inc"))
+  (sim |> mp_trajectory() |> ggplot()
+    + geom_line(aes(time, value))
+    + geom_point(aes(time, value), data = seroprevdata, colour = "red")
+    + facet_wrap(~matrix, scales = "free")
+  )
+}
+
+print(calibrator2)
+
+model_estimates = mp_tmb_coef(calibrator2, conf.int = TRUE)
+print(model_estimates)
 
 rdsSave(calibrator)
 
--- scripts/timevar_spec.R
+++ scripts/timevar_spec.R
@@ -46,6 +46,11 @@ timevar_spec <- spec |> mp_tmb_insert(
     , integers = list(double_vac_changepoints = double_vac_changepoints, booster_vac_changepoints = booster_vac_changepoints)
 )
 
+## inserting betas?
+
+
+
+
 if(reporting_delay){
 	timevar_spec = mp_tmb_insert_reports(timevar_spec
 					     , incidence_name = "inc_total"
