9117daf2d298680ccecae701455b1621b2ff3c2c
stevencarlislewalker <stevencarlislewalker@gmail.com>
2025-06-30 16:18:57 -0400

    sigh

--- experiments/seir-test-for-mike.R
+++ experiments/seir-test-for-mike.R
@@ -1,44 +1,49 @@
 library(macpan2); library(dplyr); library(tidyr); library(ggplot2); library(splines)
-seir = mp_tmb_library("starter_models", "seir", package = "macpan2")
+seir = (mp_tmb_library("starter_models", "seir", package = "macpan2")
+  |> mp_tmb_insert("before", at = 1L, expressions = list(beta ~ beta0), default = list(beta0 = 0.2))
+)
 time_steps = 100
 basis_cols = 4
 X = splines::ns(1:time_steps
   , basis_cols
-  , intercept = TRUE
+  , intercept = FALSE
 )
 seir_tv = mp_tmb_insert_glm_timevar(seir
   , parameter_name = "beta"
   , design_matrix = X
-  , timevar_coef = c(0.3, -0.3, 0.2, 0.1)
+  , timevar_coef = c(1.3, -3.3, 1.2, 1.1)
   , link_function = mp_log
   , timevar_coef_name = "beta_values"
 )
 seir_tv_sim = (seir_tv
-  |> mp_simulator(time_steps, "I")
+  |> mp_simulator(time_steps, c("I", "beta"))
   |> mp_trajectory()
 )
 calibrator = mp_tmb_calibrator(seir_tv
   , data = seir_tv_sim
   , traj = list(I = mp_poisson())
   , par = list(
-      beta_values = mp_uniform()
+      beta_values = mp_normal(0, 5)
+    , log_beta0 = mp_normal(log(0.1), 1)
   )
   , default = list(
       beta_values = rep(0, basis_cols)
   )
-  , outputs = "log_I"
+  , outputs = c("log_I", "log_beta")
 )
 
 mp_optimize(calibrator)
 seir_tv_fit = mp_trajectory_sd(calibrator, conf.int = TRUE)
 plot = (ggplot()
   + geom_line(aes(time, value), data = seir_tv_sim)
+  + geom_line(aes(time, value), data = seir_tv_fit, colour = "red")
   + geom_ribbon(aes(time, ymin = conf.low, ymax = conf.high)
     , data = seir_tv_fit
     , colour = "red"
     , alpha = 0.4
     , fill = "red"
   )
+  + facet_wrap(~matrix, scales = 'free')
   + theme_bw()
 )
 print(plot)
