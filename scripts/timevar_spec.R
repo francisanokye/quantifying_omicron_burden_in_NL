a1968be65470a58dd4e899eb0fc9a8fd0ff41c14
stevencarlislewalker <stevencarlislewalker@gmail.com>
2025-08-15 10:11:27 -0400

    mike

--- scripts/timevar_spec.R
+++ scripts/timevar_spec.R
@@ -6,6 +6,8 @@ loadEnvironments()
 
 dat <- rdsRead()
 
+print(dat)
+
 double_daily_vac = dat |> dplyr::filter(type == "numtotal_fully")
 booster_daily_vac = dat |> dplyr::filter(type == "numtotal_additional")
 
@@ -15,7 +17,7 @@ initialize_state = list(
 	A1 ~ 1, E1 ~ 1, I1 ~ 1, R1 ~ 1
 	, A2 ~ 1, E2 ~ 1, I2 ~ 1, R2 ~ 1
 	, A3 ~ 1, E3 ~ 1, I3 ~ 1, R3 ~ 1
-	, V2 ~ 1
+	, V2 ~ 1 + mike
 	, V3 ~ 1
 	, S1 ~ N - A1 - E1 - I1 - R1 - V2 - A2 - E2 - I2 - R2 - V3 - A3 - E3 - I3 - R3
 )
